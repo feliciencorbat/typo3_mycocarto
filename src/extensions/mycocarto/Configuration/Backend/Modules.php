49cc8bde1d223132a0e913a89607291b654b6be4
Félicien Corbat <felicien@champis.net>
2023-06-27 14:10:00 +0200

    Improve module name and icon

--- src/extensions/mycocarto/Configuration/Backend/Modules.php
+++ src/extensions/mycocarto/Configuration/Backend/Modules.php
@@ -2,15 +2,23 @@
 
 use Feliciencorbat\Mycocarto\Controller\EcologyController;
 
+
 return [
+    'mycocarto' => [
+        'labels' => ['title' => 'MycoCarto'],
+        'position' => ['after' => 'web'],
+        'iconIdentifier' => 'tx_mycocarto_logo',
+        'navigationComponent' => 'TYPO3/CMS/Backend/PageTree/PageTreeElement',
+    ],
+
     'tx_mycocarto_ecologies' => [
-        'parent' => 'web',
-        'position' => ['after' => 'web_info'],
+        'parent' => 'mycocarto',
         'access' => 'user',
         'workspaces' => 'live',
         'path' => '/module/web/ecologies',
-        'labels' => ['Ecologie', 'Ecologie', 'Ecologie'],
+        'labels' => ['title' => 'Ecologies'],
         'extensionName' => 'mycocarto',
+        'iconIdentifier' => 'tx_mycocarto_logo',
         'controllerActions' => [
             EcologyController::class => [
                 'list', 'new', 'create', 'edit', 'update', 'delete'
---
+++ src/extensions/mycocarto/Configuration/Icons.php
@@ -0,0 +1,10 @@
+<?php
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'tx_mycocarto_logo' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:mycocarto/Resources/Public/Icons/icon_mycocarto.svg',
+    ]
+];
