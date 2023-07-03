490cff6c5484f6d9b01267694286ce9abd1ee74a
Félicien Corbat <felicien@champis.net>
2023-07-03 12:28:11 +0200

    Add unique constraints to tables

--- src/extensions/mycocarto/ext_tables.sql
+++ src/extensions/mycocarto/ext_tables.sql
@@ -1,52 +1,51 @@
 CREATE TABLE tx_mycocarto_domain_model_ecology
 (
-    name varchar(255) NOT NULL
+    name varchar(255) NOT NULL,
+    UNIQUE KEY unique_ecology_name (name, deleted)
 );
 
 CREATE TABLE tx_mycocarto_domain_model_tree
 (
-    name varchar(255) NOT NULL,
-    scientific_name varchar(255) NOT NULL
+    name            varchar(255) NOT NULL,
+    scientific_name varchar(255) NOT NULL,
+    UNIQUE KEY unique_tree_name (name, deleted),
+    UNIQUE KEY unique_tree_scientific_name (scientific_name, deleted)
 );
 
 CREATE TABLE tx_mycocarto_domain_model_species
 (
-    genus varchar(255) NOT NULL,
-    species varchar(255) NOT NULL,
-    author varchar(255),
-    family int(11) unsigned NOT NULL,
+    genus   varchar(255)     NOT NULL,
+    species varchar(255)     NOT NULL,
+    author  varchar(255),
+    family  int(11) unsigned NOT NULL,
+    UNIQUE KEY unique_species_genus_species (genus, species, deleted),
     KEY index_family (family)
 );
 
 CREATE TABLE tx_mycocarto_domain_model_taxon
 (
-    scientific_name varchar(255) NOT NULL,
-    parent_taxon int(11) unsigned,
-    taxon_level int(11) unsigned NOT NULL,
+    scientific_name varchar(255)     NOT NULL,
+    parent_taxon    int(11) unsigned,
+    taxon_level     int(11) unsigned NOT NULL,
+    UNIQUE KEY unique_taxon_scientific_name_level (scientific_name, taxon_level, deleted),
     KEY index_parent_taxon (parent_taxon),
     KEY index_taxon_level (taxon_level)
 );
 
 CREATE TABLE tx_mycocarto_domain_model_taxonlevel
 (
-    name varchar(255) NOT NULL
+    name varchar(255) NOT NULL,
+    UNIQUE KEY unique_taxon_level_name (name, deleted)
 );
 
 CREATE TABLE tx_mycocarto_domain_model_observation
 (
-    date int(10) NOT NULL,
-    latitude float NOT NULL,
-    longitude float NOT NULL,
-    ecology int(11) unsigned NOT NULL,
-    species int(11) unsigned NOT NULL,
+    date      int(10)          NOT NULL,
+    latitude  float            NOT NULL,
+    longitude float            NOT NULL,
+    ecology   int(11) unsigned NOT NULL,
+    species   int(11) unsigned NOT NULL,
+    UNIQUE KEY unique_observation (date, latitude, longitude, ecology, species, deleted),
     KEY index_ecology (ecology),
     KEY index_species (species)
-);
-
-CREATE TABLE tx_mycocarto_domain_observation_tree
-(
-    observation int(11) unsigned NOT NULL,
-    tree int(11) unsigned NOT NULL,
-    KEY index_observation (observation),
-    KEY index_tree (tree)
 );
