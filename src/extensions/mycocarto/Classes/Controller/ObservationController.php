fc99026e9c69e85b74d986670c49a003cafde7ac
feliciencorbat <felicien.corbat@ideative.ch>
2023-08-07 16:38:29 +0200

    Debug redirect (avoid redirect loop)

--- src/extensions/mycocarto/Classes/Controller/ObservationController.php
+++ src/extensions/mycocarto/Classes/Controller/ObservationController.php
@@ -102,7 +102,7 @@ class ObservationController extends ActionController
             return $this->htmlResponse();
         } catch (Exception $e) {
             $this->addFlashMessage($e->getMessage(), 'Erreur', ContextualFeedbackSeverity::ERROR);
-            return $this->redirect('list');
+            return $this->redirectToUri('/');
         }
     }
 
